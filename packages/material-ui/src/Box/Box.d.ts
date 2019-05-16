378c28a7e7aa6a3ba61fd6c2211b9796ac188745
Dylan Jager-Kujawa <dylan.jagerkujawa@sial.com>
2019-05-15 19:33:05 -0500

    [Box] Accept responsive values and variables in Box props
    Box prop types previously didn't allow for responsive values (e.g. arrays/objects) and also did not account for variables in fontWeight value.
    
    fontWeight is now a string value to accept variable names without compiler errors, as opposed to the union type defined in React.CSSProperties.

--- packages/material-ui/src/Box/Box.d.ts
+++ packages/material-ui/src/Box/Box.d.ts
@@ -1,9 +1,27 @@
 import * as React from 'react';
+import { Omit } from '@material-ui/types';
+
+type ResponsiveValue<Value = string | number> =
+  | Value
+  | [Value, Value]
+  | [Value, Value, Value]
+  | [Value, Value, Value, Value]
+  | [Value, Value, Value, Value, Value]
+  | {
+      xs?: Value;
+      sm?: Value;
+      md?: Value;
+      lg?: Value;
+      xl?: Value;
+    };
+
+type ResponsiveCSSProperties<PropName extends keyof React.CSSProperties> = {
+  [Name in PropName]?: ResponsiveValue<React.CSSProperties[Name]>
+};
 
 export interface BoxProps
-  extends React.HTMLAttributes<HTMLElement>,
-    Pick<
-      React.CSSProperties,
+  extends Omit<React.HTMLAttributes<HTMLElement>, 'color'>,
+    ResponsiveCSSProperties<
       | 'alignContent'
       | 'alignItems'
       | 'alignSelf'
@@ -26,7 +44,6 @@ export interface BoxProps
       | 'flexWrap'
       | 'fontFamily'
       | 'fontSize'
-      | 'fontWeight'
       | 'height'
       | 'justifyContent'
       | 'left'
@@ -47,23 +64,27 @@ export interface BoxProps
   // styled API
   clone?: boolean;
   // Box specific props
-  bgcolor?: string;
-  displayPrint?: string;
-  m?: string | number;
-  mb?: string | number;
-  ml?: string | number;
-  mr?: string | number;
-  mt?: string | number;
-  mx?: string | number;
-  my?: string | number;
-  order?: string | number;
-  p?: string | number;
-  pb?: string | number;
-  pl?: string | number;
-  pr?: string | number;
-  pt?: string | number;
-  px?: string | number;
-  py?: string | number;
+  bgcolor?: ResponsiveValue<string>;
+  displayPrint?: ResponsiveValue<string>;
+  m?: ResponsiveValue;
+  mb?: ResponsiveValue;
+  ml?: ResponsiveValue;
+  mr?: ResponsiveValue;
+  mt?: ResponsiveValue;
+  mx?: ResponsiveValue;
+  my?: ResponsiveValue;
+  order?: ResponsiveValue;
+  p?: ResponsiveValue;
+  pb?: ResponsiveValue;
+  pl?: ResponsiveValue;
+  pr?: ResponsiveValue;
+  pt?: ResponsiveValue;
+  px?: ResponsiveValue;
+  py?: ResponsiveValue;
+  // CSSProperty Overrides
+  // Accept a string | number here instead of the union type in CSSProperties
+  // so this prop can accept theme variable names, e.g. fontWeightBold.
+  fontWeight?: ResponsiveValue;
 }
 
 declare const Box: React.ComponentType<BoxProps>;
